e1e904dd235d64f58da3101fec194163abf2bb3b
Wanderson Rodrigues <wanderson.rodrigues.frontend@outlook.com.br>
2020-08-20 21:14:19 -0300

    Exercicos de programcao

---
+++ Exerciciso-01.js
@@ -0,0 +1,59 @@
+// Declarar uma variável chamada `myvar`, sem valor.
+
+let myvar = ;
+
+// Após declarada, atribua o valor 10 à variável `myvar`.
+
+let myvar = 10;
+
+// Declare uma nova variável chamada `soma`, e adicione uma instrução somando os valores 15 e 8.
+
+const soma = 15 + 8;
+
+// Atribua à variável `soma` todo o valor dela, somando 1, usando o operador de soma abreviado.
+
+const soma = soma + 1;
+const soma++;
+
+// Atribua à variável `soma` todo o valor dela, multiplicando por 3, usando o operador de multiplicação abreviado.
+
+const soma = soma * 3;
+const soma *= 3;
+
+// Qual é o valor da variável `soma` até aqui?
+
+console.log(soma)
+R = 102;
+
+// Declare uma variável chamada `souninja`, atribuindo à ela o valor booleano que representa `verdadeiro`.
+
+const souninja = true;
+
+// Declare uma variável chamada `comida` que recebe um array com os valores 'arroz', 'feijão' e 'ovo'.
+
+const comida = ['arroz', 'feijão','ovo']
+
+// Digite a instrução que imprime o valor de 'feijao', que está na variável `comida`.
+
+comida[1];
+
+// Digite o código que verifica se a variável `soma' é igual a variável `myvar` (testando também o tipo).
+
+if(soma === myvar){
+	console.log("ok sao iguais em variaveis e tipo")
+}else{
+	console.log("Nao sao igual e nem do mesmo tipo")
+}
+// Digite o código que verifica se a variável `myvar` é menor ou igual à variável `soma`.
+
+soma <= myvar
+
+// Crie uma função chamada `divisao` que receba como parâmetro dois números, 
+// e retorne o resultado da divisão entre eles.
+
+function divisao(a,b){
+	return a/b;
+}
+divisao(10,5);
+// Invoque a função criada acima, passando os parâmetros 10 e 2.
+divisao(10,5);
